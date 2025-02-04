1ac4e30bb15d80d83c08244ad63636fc7cea94ab
Bandi Satyanarayana <satyaa6644@gmail.com>
2025-02-04 15:14:44 -0500

    Initial commit with setup and tests

---
+++ tests/test_calculator.py
@@ -0,0 +1,11 @@
+"""
+Unit tests for the calculator module.
+"""
+
+def test_addition():
+    """Tests addition function"""
+    assert 2 + 3 == 5  # This should pass
+
+def test_subtraction():
+    """Tests subtraction function"""
+    assert 5 - 2 == 3  # This should pass
