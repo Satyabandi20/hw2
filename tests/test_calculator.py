a2d2ed8e40390f91a38ea865d978292c71fa085a
Satyabandi20 <satyaa6644@gmail.com>
2025-02-06 10:56:45 -0500

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,14 +1,11 @@
-"""This is a test file for my calculator program""" 
-from calculator import add  
-'''My Calculator Test'''
-from calculator import add, subtract
+"""This is a test file for my calculator program"""
+
+from calculator import add, subtract  # Import the required functions
 
 def test_addition():
-    """The is a test of the addition function"""
-    '''Test that addition function works '''    
-assert add(2,2) == 4
+    """Test that addition function works"""  
+    assert add(2, 2) == 4  # ✅ Fixed indentation
 
 def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
-    
+    """Test that subtraction function works"""  
+    assert subtract(2, 2) == 0  # ✅ Correct function name
